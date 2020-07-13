00a2fd67485939cf0b520ea956225d6132fc1b4b
saitirumala <saiprasadtirumala.t@gmail.com>
2020-07-13 18:20:06 +0530

    Updates

---
+++ precache-manifest.efc6ded797060aea91fdb4d9bfaaca69.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6f3733bcaa1795a6ea61466c05e7d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c817a5360498afaf96b1",
+    "url": "/static/css/2.b75c77fa.chunk.css"
+  },
+  {
+    "revision": "879e6f94f121ce0b82c6",
+    "url": "/static/css/main.a43314fb.chunk.css"
+  },
+  {
+    "revision": "c817a5360498afaf96b1",
+    "url": "/static/js/2.c72bbe24.chunk.js"
+  },
+  {
+    "revision": "e8eb7399512ac04bf960b63fa6be489c",
+    "url": "/static/js/2.c72bbe24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879e6f94f121ce0b82c6",
+    "url": "/static/js/main.20123759.chunk.js"
+  },
+  {
+    "revision": "a96a22f602094e2b1187",
+    "url": "/static/js/runtime-main.0c219e1c.js"
+  },
+  {
+    "revision": "44e38abfb776c130e17de3699c84e9e0",
+    "url": "/static/media/bike1.44e38abf.png"
+  },
+  {
+    "revision": "3513f88f0d5a5d75922a4f26532c2ea0",
+    "url": "/static/media/bike4.3513f88f.png"
+  },
+  {
+    "revision": "424008903d0c30cc263620bb565d36f9",
+    "url": "/static/media/contact.42400890.jpg"
+  },
+  {
+    "revision": "cf0e3c39a8ea78ffc0f32c96799400d4",
+    "url": "/static/media/contactLogo.cf0e3c39.png"
+  },
+  {
+    "revision": "7b9935fe8f6523cc383561d865e226f2",
+    "url": "/static/media/gmail.7b9935fe.png"
+  },
+  {
+    "revision": "8c955460d30774293a39ba49f551e6db",
+    "url": "/static/media/insta-logo.8c955460.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3f4281af681f8d75aa7f4dce4afbfe15",
+    "url": "/static/media/profile.3f4281af.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "a7b4e2839fc37a24868a6ff0ab523141",
+    "url": "/static/media/skyinLogo.a7b4e283.jpeg"
+  },
+  {
+    "revision": "30626cee8077299493ae7208cf1fee77",
+    "url": "/static/media/vehicle1.30626cee.png"
+  },
+  {
+    "revision": "9278a6321d44502b3e71fb12846535c2",
+    "url": "/static/media/vehicle2.9278a632.png"
+  },
+  {
+    "revision": "0db14c853c3a9afdea507eed780161fb",
+    "url": "/static/media/vehicle3.0db14c85.png"
+  },
+  {
+    "revision": "7f99f91a3b5e3096cec65dd7ec3c640a",
+    "url": "/static/media/vehicle4.7f99f91a.png"
+  },
+  {
+    "revision": "482b3dc56c38c9133241570828c482f8",
+    "url": "/static/media/vehicle5.482b3dc5.png"
+  }
+]);
